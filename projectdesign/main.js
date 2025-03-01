7226d13d53dd86b0dcdddc1c1cc606c02ab2acc9
olamile100t <emmanuelfagbemide@gmail.com>
2025-03-01 10:51:01 +0100

    pojectresponsive

---
+++ projectdesign/main.js
@@ -0,0 +1,10 @@
+console.log ("Hello World")
+
+let media = twitter
+
+console.log("my favorite media" + media);
+console.log(`my favorite media $(media)`);
+
+
+
+
---
+++ projectdesign/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import(‘tailwindcss’).config} */
+
+module.exports = {
+    content: [ 
+       './index.html',
+     './src/**/*.{js,ts,jux,tax}' ,
+    ],
+     theme: {
+        extend: {},
+    },
+    plugins:  [],
+  }
